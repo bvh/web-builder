ddc610db41fd34a334f2e301e91ccccb8417a9d0
Brian Van Horne <code@brianvanhorne.com>
2026-04-06 19:55:11 -0700

    nodes now report dirs to make, copy paths, and file write paths

--- src/web_builder/builder.py
+++ src/web_builder/builder.py
@@ -6,11 +6,19 @@ from web_builder.node import Node
 log = logging.getLogger("web-builder")
 
 
-def build_target(target: str, node: Node, source_root: str | None = None) -> None:
-    if not source_root:
-        source_root = node.source
-        log.info(f"*** source_root={str(source_root)}")
-    log.info(f">>> {node.source} -> {os.path.join(target, node.target_path)}")
+def build_target(target: str, node: Node) -> None:
+    log.info(f">>> {node.type}({node.source}) -> {target}")
+    dir_target = node.directory_target
+    if dir_target:
+        log.info(f"  >>> MAKEDIR: {os.path.join(target, dir_target)}")
+    copy_target = node.copy_target
+    if copy_target:
+        log.info(f"  >>> COPY:    {node.source} -> {os.path.join(target, copy_target)}")
+    content_target = node.content_target
+    if content_target:
+        log.info(
+            f"  >>> WRITE:   {node.content_source} -> {os.path.join(target, content_target)}"
+        )
     for child in node.children:
-        build_target(target, child, source_root=source_root)
+        build_target(target, child)
     return
--- src/web_builder/node.py
+++ src/web_builder/node.py
@@ -1,8 +1,17 @@
+from enum import StrEnum
 import logging
 import os
 from pathlib import Path
 
 log = logging.getLogger("web-builder")
+
+
+class NodeType(StrEnum):
+    HOME = "HOME"
+    DIRECTORY = "DIRECTORY"
+    PAGE = "PAGE"
+    IMAGE = "IMAGE"
+    STATIC = "STATIC"
 
 
 class Node:
@@ -12,7 +21,8 @@ class Node:
         self.target_path = ""
         if self.parent:
             parent.add_child(self)
-            self.target_path = os.path.join(parent.target_path, self.source.name)
+            self.target_path = Path(os.path.join(parent.target_path, self.source.name))
+        self.type = self._get_type()
         self.children = []
         self.config_source = None
         self.content_source = None
@@ -28,3 +38,49 @@ class Node:
 
     def add_config(self, path: Path) -> None:
         self.config_source = path
+
+    def _get_type(self) -> NodeType:
+        if self.source.is_dir():
+            if self.parent is None:
+                type = NodeType.HOME
+            else:
+                type = NodeType.DIRECTORY
+        else:
+            if self.source.suffix in [".md"]:
+                type = NodeType.PAGE
+            elif self.source.suffix in [".jpg", ".jpeg"]:
+                type = NodeType.IMAGE
+            else:
+                type = NodeType.STATIC
+        return type
+
+    @property
+    def directory_target(self) -> Path | None:
+        if self.type in [NodeType.DIRECTORY]:
+            path = self.target_path
+        elif self.type in [NodeType.IMAGE, NodeType.PAGE]:
+            # always use pretty URLs
+            path = self.target_path.with_name(self.source.stem)
+        else:
+            path = None
+        return path
+
+    @property
+    def copy_target(self) -> Path | None:
+        if self.type in [NodeType.IMAGE]:
+            path = Path(os.path.join(self.directory_target, self.source.name))
+        elif self.type in [NodeType.STATIC]:
+            path = self.target_path
+        else:
+            path = None
+        return path
+
+    @property
+    def content_target(self) -> Path | None:
+        if self.type in [NodeType.DIRECTORY, NodeType.IMAGE, NodeType.PAGE]:
+            path = Path(os.path.join(self.directory_target, "index.html"))
+        elif self.type in [NodeType.HOME]:
+            path = Path("index.html")
+        else:
+            path = None
+        return path

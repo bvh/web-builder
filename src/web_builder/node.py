ccf59ede4fb22d385559aa5ffe1ee8f671aab0b3
Brian Van Horne <code@brianvanhorne.com>
2026-05-16 14:52:53 -0700

    improved node parsing

--- src/web_builder/node.py
+++ src/web_builder/node.py
@@ -3,19 +3,50 @@ from pathlib import Path
 
 
 class Node:
-    def __init__(self, path: str) -> None:
+    def __init__(self, path: str, parent: Node = None) -> None:
         self.path = Path(path)
+        self.parent = parent
         self.content = None
+        self.config = None
         self.children = []
-        if self.path.is_dir():
+        self.files = []
+
+        if self.path.is_file() and self.path.name.endswith(".md"):
+            # If the node path is a markdown file, then that markdown file
+            # is the node's content. Nothing else to do.
+            self.content = self.path
+
+        elif self.path.is_dir():
+            # If the node is a directory, then we need to look for content,
+            # config, and child nodes within that directory.
             with os.scandir(self.path) as entries:
                 for entry in entries:
-                    if entry.is_file() and entry.name == "index.md":
-                        self.content = entry.path
-                    elif entry.is_file() and entry.name.endswith(".md"):
-                        self.children.append(Node(entry.path))
-                    elif entry.is_dir():
-                        self.children.append(Node(entry.path))
+                    # skip sybolic links and dotfiles
+                    if not (entry.name.startswith(".") or entry.is_symlink()):
+                        if entry.is_dir():
+                            # This will recursively scan the child directory.
+                            # For now, empty directories will be added as nodes,
+                            # and we'll allow the build step to decide whether
+                            # to include it in the final site or not.
+                            self.children.append(Node(entry.path, parent=self))
+                        # We've already ruled out symlinks, and above clause
+                        # covers directories, so we can assume everything else
+                        # is a regular file.
+                        elif entry.name == "config.json":
+                            # local configuration for this node
+                            self.config = Path(entry.path)
+                        elif entry.name == "index.md":
+                            # markdown content for this node
+                            self.content = Path(entry.path)
+                        elif entry.name.endswith(".md"):
+                            # child pages within this node
+                            self.children.append(Node(entry.path, parent=self))
+                        else:
+                            # a static file that will need to be copied
+                            self.files.append(Path(entry.path))
+
+        else:
+            raise ValueError(f"ERROR: {self.path} is not a markdown file or directory")
 
     def __str__(self) -> str:
         return f"Node(path={self.path}, content={self.content}, children={len(self.children)})"
